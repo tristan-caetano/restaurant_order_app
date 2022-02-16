eb8129636011b3f6ea8ab2b14bcc590b18059900
Tristan Caetano <tristancostacaetano@gmail.com>
2022-02-16 14:39:28 -0500

    Completed second view, started summary view

--- app/src/main/java/com/example/restaurant_order_app/checkoutActivity.java
+++ app/src/main/java/com/example/restaurant_order_app/checkoutActivity.java
@@ -2,13 +2,78 @@ package com.example.restaurant_order_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class checkoutActivity extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String BBQ = "bbq";
+    public static final String BUFFALO = "buffalo";
+    public static final String PLAIN = "plain";
+    public int buffalo, bbq, plain;
+    public double total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        // Creating shared prefs
+        SharedPreferences sharedPrefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        bbq = sharedPrefs.getInt(BBQ, 0);
+        buffalo = sharedPrefs.getInt(BUFFALO, 0);
+        plain = sharedPrefs.getInt(PLAIN, 0);
+
+        // Declaring buttons
+        Button orderEditButton = (Button) findViewById(R.id.orderEditButton);
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+
+        // Declaring check boxes
+        CheckBox sourceCheck = (CheckBox) findViewById(R.id.sourceCheck);
+        CheckBox tip15Check = (CheckBox) findViewById(R.id.tip15Check);
+        CheckBox tip20Check = (CheckBox) findViewById(R.id.tip20Check);
+
+        // Declaring text views for summary
+        TextView netSummaryView = (TextView) findViewById(R.id.netSummaryView);
+        TextView finalSummaryView = (TextView) findViewById(R.id.finalSummaryView);
+
+        // Declaring edit text field for tip
+        EditText tipEdit = (EditText) findViewById(R.id.tipEdit);
+
+        String netSummary = fillNetView();
+
+        netSummaryView.setText(netSummary);
+    }
+
+    public String fillNetView(){
+
+        String returnView = "";
+        total = ((9.99 * plain) + (12.99 * buffalo) + (15.99 * bbq));
+
+        System.out.println("BBQ: " + bbq + "\n");
+        System.out.println("Buffalo: " + buffalo + "\n");
+        System.out.println("Plain: " + plain + "\n");
+
+        if(bbq > 0){
+            returnView += ("BBQ Wings x " + bbq + "                          $" + (15.99 * bbq) + "\n\n");
+        }
+
+        if(buffalo > 0){
+            returnView += ("Buffalo Wings x " + buffalo + "                     $" + (12.99 * buffalo) + "\n\n");
+        }
+
+        if(plain > 0){
+            returnView += ("Plain Wings x " + plain + "                         $" + (9.99 * plain) + "\n\n");
+        }
+
+        returnView += "_______________________________________\n";
+        returnView += "Pre Tip Total:                             $" + ((9.99 * plain) + (12.99 * buffalo) + (15.99 * bbq));
+
+        return returnView;
     }
 }
--- app/src/main/java/com/example/restaurant_order_app/menuActivity.java
+++ app/src/main/java/com/example/restaurant_order_app/menuActivity.java
@@ -3,11 +3,13 @@ package com.example.restaurant_order_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -17,11 +19,18 @@ public class menuActivity extends AppCompatActivity {
     public static final String BBQ = "bbq";
     public static final String BUFFALO = "buffalo";
     public static final String PLAIN = "plain";
+    private int buffalo, bbq, plain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        // Creating shared prefs
+        SharedPreferences sharedPrefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        bbq = sharedPrefs.getInt(BBQ, 0);
+        buffalo = sharedPrefs.getInt(BUFFALO, 0);
+        plain = sharedPrefs.getInt(PLAIN, 0);
 
         // Declaring plus and minus buttons for food quantities
         Button bbqPlus = (Button) findViewById(R.id.bbqPlus);
@@ -92,6 +101,14 @@ public class menuActivity extends AppCompatActivity {
             }
         });
 
+        // Goes to checkout screen with values
+        checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkoutValidation(bbqEdit, buffEdit, plainEdit);
+            }
+        });
+
     }
 
     // Method that controls how the order values are changed
@@ -111,5 +128,39 @@ public class menuActivity extends AppCompatActivity {
 
         // Setting integer back to string and putting it back into the edit text
         wingEdit.setText(Integer.toString(currentVal), TextView.BufferType.EDITABLE);
+    }
+
+    // Making sure the user is ready for checkout
+    void checkoutValidation(EditText bbqE, EditText buffE, EditText plainE){
+
+        // Declaring shared prefs
+        SharedPreferences sharedPrefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+
+        // Declaring toast
+        Toast toast = null;
+
+        // Getting wing values from edit texts
+        bbq = Integer.valueOf(bbqE.getText().toString());
+        buffalo = Integer.valueOf(buffE.getText().toString());
+        plain = Integer.valueOf(plainE.getText().toString());
+
+        // Making sure the user selected wings
+        if(bbq > 0 || buffalo > 0 || plain > 0){
+
+            // Saving values
+            editor.putInt(BBQ, bbq);
+            editor.putInt(BUFFALO, buffalo);
+            editor.putInt(PLAIN, plain);
+            editor.commit();
+
+            // Going to checkout screen
+            Intent checkoutIntent = new Intent(getApplicationContext(), checkoutActivity.class);
+            startActivity(checkoutIntent);
+
+        }else {
+            toast = Toast.makeText(getApplicationContext(), "Your cart is empty.", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
